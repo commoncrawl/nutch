d66786ff9502578c5f917712ee2164c9433b4b2d
Sebastian Nagel <sebastian@commoncrawl.org>
2017-01-14 17:53:58 +0100

    - update urlnormalizer-basic to latest version - remove urlnormalizer-fast:   * 30% faster than urlnormalizer-basic   * but less evolved: does not normalize percent escapes

--- src/plugin/urlnormalizer-basic/src/java/org/apache/nutch/net/urlnormalizer/basic/BasicURLNormalizer.java
+++ src/plugin/urlnormalizer-basic/src/java/org/apache/nutch/net/urlnormalizer/basic/BasicURLNormalizer.java
@@ -17,76 +17,80 @@
 
 package org.apache.nutch.net.urlnormalizer.basic;
 
-import java.net.URL;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-// Slf4j Logging imports
+import org.apache.hadoop.conf.Configured;
+import org.apache.nutch.net.URLNormalizer;
+import org.apache.nutch.net.URLNormalizers;
+import org.apache.nutch.util.NutchConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// Nutch imports
-import org.apache.nutch.net.URLNormalizer;
-
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.conf.Configured;
-import org.apache.oro.text.regex.*;
-
-/** Converts URLs to a normal form . */
+/**
+ * Converts URLs to a normal form:
+ * <ul>
+ * <li>remove dot segments in path: <code>/./</code> or <code>/../</code></li>
+ * <li>remove default ports, e.g. 80 for protocol <code>http://</code></li>
+ * <li>normalize <a href=
+ * "https://en.wikipedia.org/wiki/Percent-encoding#Percent-encoding_in_a_URI">
+ * percent-encoding</a> in URL paths</li>
+ * </ul>
+ */
 public class BasicURLNormalizer extends Configured implements URLNormalizer {
-    public static final Logger LOG = LoggerFactory.getLogger(BasicURLNormalizer.class);
+  public static final Logger LOG = LoggerFactory
+      .getLogger(BasicURLNormalizer.class);
 
-    private Perl5Compiler compiler = new Perl5Compiler();
-    private ThreadLocal<Perl5Matcher> matchers = new ThreadLocal<Perl5Matcher>() {
-        protected Perl5Matcher initialValue() {
-          return new Perl5Matcher();
-        }
-      };
-    private final Rule relativePathRule;
-    private final Rule leadingRelativePathRule;
-    private final Rule currentPathRule;
-    private final Rule adjacentSlashRule;
+  /**
+   * Pattern to detect whether a URL path could be normalized. Contains one of
+   * /. or ./ /.. or ../ //
+   */
+  private final static Pattern hasNormalizablePathPattern = Pattern
+      .compile("/[./]|[.]/");
 
-    private Configuration conf;
+  /**
+   * Nutch 1098 - finds URL encoded parts of the URL
+   */
+  private final static Pattern unescapeRulePattern = Pattern
+      .compile("%([0-9A-Fa-f]{2})");
 
-    public BasicURLNormalizer() {
-      try {
-        // this pattern tries to find spots like "/xx/../" in the url, which
-        // could be replaced by "/" xx consists of chars, different then "/"
-        // (slash) and needs to have at least one char different from "."
-        relativePathRule = new Rule();
-        relativePathRule.pattern = (Perl5Pattern)
-          compiler.compile("(/[^/]*[^/.]{1}[^/]*/\\.\\./)",
-                           Perl5Compiler.READ_ONLY_MASK);
-        relativePathRule.substitution = new Perl5Substitution("/");
+  // charset used for encoding URLs before escaping
+  private final static Charset utf8 = Charset.forName("UTF-8");
 
-        // this pattern tries to find spots like leading "/../" in the url,
-        // which could be replaced by "/"
-        leadingRelativePathRule = new Rule();
-        leadingRelativePathRule.pattern = (Perl5Pattern)
-          compiler.compile("^(/\\.\\./)+", Perl5Compiler.READ_ONLY_MASK);
-        leadingRelativePathRule.substitution = new Perl5Substitution("/");
-
-        // this pattern tries to find spots like "/./" in the url,
-        // which could be replaced by "/"
-        currentPathRule = new Rule();
-        currentPathRule.pattern = (Perl5Pattern)
-          compiler.compile("(/\\./)", Perl5Compiler.READ_ONLY_MASK);
-        currentPathRule.substitution = new Perl5Substitution("/");
-
-        // this pattern tries to find spots like "xx//yy" in the url,
-        // which could be replaced by a "/"
-        adjacentSlashRule = new Rule();
-        adjacentSlashRule.pattern = (Perl5Pattern)      
-          compiler.compile("/{2,}", Perl5Compiler.READ_ONLY_MASK);     
-        adjacentSlashRule.substitution = new Perl5Substitution("/");
-        
-      } catch (MalformedPatternException e) {
-        throw new RuntimeException(e);
+  /** look-up table for characters which should not be escaped in URL paths */
+  private final static boolean[] unescapedCharacters = new boolean[128];
+  static {
+    for (int c = 0; c < 128; c++) {
+      /* https://tools.ietf.org/html/rfc3986#section-2.2
+       * For consistency, percent-encoded octets in the ranges of ALPHA
+       * (%41-%5A and %61-%7A), DIGIT (%30-%39), hyphen (%2D), period (%2E),
+       * underscore (%5F), or tilde (%7E) should not be created by URI
+       * producers and, when found in a URI, should be decoded to their
+       * corresponding unreserved characters by URI normalizers.
+       */
+      if ((0x41 <= c && c <= 0x5A)
+        || (0x61 <= c && c <= 0x7A)
+        || (0x30 <= c && c <= 0x39)
+        || c == 0x2D || c == 0x2E
+        || c == 0x5F || c == 0x7E) {
+        unescapedCharacters[c] = true;
+      } else {
+        unescapedCharacters[c] = false;
+      }
       }
     }
 
     public String normalize(String urlString, String scope)
             throws MalformedURLException {
+    
         if ("".equals(urlString))                     // permit empty
             return urlString;
 
@@ -104,14 +108,22 @@ public class BasicURLNormalizer extends Configured implements URLNormalizer {
         if (!urlString.startsWith(protocol))        // protocol was lowercased
             changed = true;
 
-        if ("http".equals(protocol) || "https".equals(protocol) || "ftp".equals(protocol)) {
+    if ("http".equals(protocol) || "https".equals(protocol)
+        || "ftp".equals(protocol)) {
 
-            if (host != null) {
-                String newHost = host.toLowerCase();    // lowercase host
+      if (host != null && url.getAuthority() != null) {
+        String newHost = host.toLowerCase(Locale.ROOT); // lowercase host
                 if (!host.equals(newHost)) {
                     host = newHost;
+          changed = true;
+        } else if (!url.getAuthority().equals(newHost)) {
+          // authority (http://<...>/) contains other elements (port, user,
+          // etc.) which will likely cause a change if left away
                     changed = true;
                 }
+      } else {
+        // no host or authority: recompose the URL from components
+        changed = true;
             }
 
             if (port == url.getDefaultPort()) {       // uses default port
@@ -128,14 +140,20 @@ public class BasicURLNormalizer extends Configured implements URLNormalizer {
                 changed = true;
             }
 
-            // check for unnecessary use of "/../"
-            String file2 = substituteUnnecessaryRelativePaths(file);
-
+      // check for unnecessary use of "/../", "/./", and "//"
+      String file2 = getFileWithNormalizedPath(url);
             if (!file.equals(file2)) {
                 changed = true;
                 file = file2;
+      }
             }
 
+    // properly encode characters in path/file using percent-encoding
+    String file2 = unescapePath(file);
+    file2 = escapePath(file2);
+    if (!file.equals(file2)) {
+      changed = true;
+      file = file2;
         }
 
         if (changed)
@@ -144,76 +162,137 @@ public class BasicURLNormalizer extends Configured implements URLNormalizer {
         return urlString;
     }
 
-    private String substituteUnnecessaryRelativePaths(String file) {
-        String fileWorkCopy = file;
-        int oldLen = file.length();
-        int newLen = oldLen - 1;
+  private String getFileWithNormalizedPath(URL url)
+      throws MalformedURLException {
+    String file;
 
-        // All substitutions will be done step by step, to ensure that certain
-        // constellations will be normalized, too
-        //
-        // For example: "/aa/bb/../../cc/../foo.html will be normalized in the
-        // following manner:
-        //   "/aa/bb/../../cc/../foo.html"
-        //   "/aa/../cc/../foo.html"
-        //   "/cc/../foo.html"
-        //   "/foo.html"
-        //
-        // The normalization also takes care of leading "/../", which will be
-        // replaced by "/", because this is a rather a sign of bad webserver
-        // configuration than of a wanted link.  For example, urls like
-        // "http://www.foo.com/../" should return a http 404 error instead of
-        // redirecting to "http://www.foo.com".
-        //
-        Perl5Matcher matcher = (Perl5Matcher)matchers.get();
+    if (hasNormalizablePathPattern.matcher(url.getPath()).find()) {
+      // only normalize the path if there is something to normalize
+      // to avoid needless work
+      try {
+        file = url.toURI().normalize().toURL().getFile();
+        // URI.normalize() does not normalize leading dot segments,
+        // see also http://tools.ietf.org/html/rfc3986#section-5.2.4
+        int start = 0;
+        while (file.startsWith("/../", start)) {
+          start += 3;
+        }
+        if (start > 0) {
+          file = file.substring(start);
+        }
+      } catch (URISyntaxException e) {
+        file = url.getFile();
+      }
+    } else {
+      file = url.getFile();
+    }
 
-        while (oldLen != newLen) {
-            // substitue first occurence of "/xx/../" by "/"
-            oldLen = fileWorkCopy.length();
-            fileWorkCopy = Util.substitute
-              (matcher, relativePathRule.pattern,
-               relativePathRule.substitution, fileWorkCopy, 1);
+    // if path is empty return a single slash
+    if (file.isEmpty()) {
+      file = "/";
+    }
 
-            // remove leading "/../"
-            fileWorkCopy = Util.substitute
-              (matcher, leadingRelativePathRule.pattern,
-               leadingRelativePathRule.substitution, fileWorkCopy, 1);
+    return file;
+  }
 
-            // remove unnecessary "/./"
-            fileWorkCopy = Util.substitute
-            (matcher, currentPathRule.pattern,
-            		currentPathRule.substitution, fileWorkCopy, 1);
+  /**
+   * Remove % encoding from path segment in URL for characters which should be
+   * unescaped according to <a
+   * href="https://tools.ietf.org/html/rfc3986#section-2.2">RFC3986</a>.
+   */
+  private String unescapePath(String path) {
+    StringBuilder sb = new StringBuilder();
             
+    Matcher matcher = unescapeRulePattern.matcher(path);
             
-            // collapse adjacent slashes with "/"
-            fileWorkCopy = Util.substitute
-            (matcher, adjacentSlashRule.pattern,
-              adjacentSlashRule.substitution, fileWorkCopy, 1);
+    int end = -1;
+    int letter;
             
-            newLen = fileWorkCopy.length();
+    // Traverse over all encoded groups
+    while (matcher.find()) {
+      // Append everything up to this group
+      sb.append(path.substring(end + 1, matcher.start()));
+      
+      // Get the integer representation of this hexadecimal encoded character
+      letter = Integer.valueOf(matcher.group().substring(1), 16);
+
+      if (letter < 128 && unescapedCharacters[letter]) {
+        // character should be unescaped in URLs
+        sb.append(new Character((char)letter));
+      } else {
+        // Append the encoded character as uppercase
+        sb.append(matcher.group().toUpperCase(Locale.ROOT));
         }
 
-        return fileWorkCopy;
+      end = matcher.start() + 2;
     }
 
+    letter = path.length();
+    
+    // Append the rest if there's anything
+    if (end <= letter - 1) {
+      sb.append(path.substring(end + 1, letter));
+    }
+
+    // Ok!
+    return sb.toString();
+  }
 
     /**
-     * Class which holds a compiled pattern and its corresponding substition
-     * string.
+   * Convert path segment of URL from Unicode to UTF-8 and escape all
+   * characters which should be escaped according to <a
+   * href="https://tools.ietf.org/html/rfc3986#section-2.2">RFC3986</a>..
      */
-    private static class Rule {
-        public Perl5Pattern pattern;
-        public Perl5Substitution substitution;
+  private String escapePath(String path) {
+    StringBuilder sb = new StringBuilder(path.length());
+
+    // Traverse over all bytes in this URL
+    for (byte b: path.getBytes(utf8)) {
+      // Is this a control character?
+      if (b < 33 || b == 91 || b == 93) {
+        // Start escape sequence 
+        sb.append('%');
+
+        // Get this byte's hexadecimal representation 
+        String hex = Integer.toHexString(b & 0xFF).toUpperCase(Locale.ROOT);
+        
+        // Do we need to prepend a zero?
+        if (hex.length() % 2 != 0 ) {
+          sb.append('0');
+          sb.append(hex);
+        } else {
+          // No, append this hexadecimal representation
+          sb.append(hex);
+        }
+      } else {
+        // No, just append this character as-is
+        sb.append((char)b);
+      }
+  }
+
+    return sb.toString();
+  }
+
+  public static void main(String args[]) throws IOException {
+    BasicURLNormalizer normalizer = new BasicURLNormalizer();
+    normalizer.setConf(NutchConfiguration.create());
+    String scope = URLNormalizers.SCOPE_DEFAULT;
+    if (args.length >= 1) {
+      scope = args[0];
+      System.out.println("Scope: " + scope);
     }
-
-
-  public void setConf(Configuration conf) {
-    this.conf = conf;
-  }
-
-  public Configuration getConf() {
-    return this.conf;
-  }
-
+    String line, normUrl;
+    BufferedReader in = new BufferedReader(
+        new InputStreamReader(System.in, utf8));
+    while ((line = in.readLine()) != null) {
+      try {
+        normUrl = normalizer.normalize(line, scope);
+        System.out.println(normUrl);
+      } catch (MalformedURLException e) {
+        System.out.println("failed: " + line);
+      }
+    }
+    System.exit(0);
 }
 
+}
