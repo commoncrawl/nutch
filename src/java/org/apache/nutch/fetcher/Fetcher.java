3270a761ee1052bed99f1b60b21d89fd5c2b1d33
Sebastian Nagel <sebastian@commoncrawl.org>
2026-08-05 12:27:29 +0200

    Fetcher: do not set number of reduce tasks programmatically
    Fix a regression introduced by NUTCH-3162 which sets the number
    of fetcher reduce tasks to 1 instead of using the value from the
    configuration property `mapreduce.job.reduces`.

--- src/java/org/apache/nutch/fetcher/Fetcher.java
+++ src/java/org/apache/nutch/fetcher/Fetcher.java
@@ -635,7 +635,6 @@ public class Fetcher extends NutchTool implements Tool {
     job.setJarByClass(Fetcher.class);
     job.setMapperClass(Fetcher.FetcherRun.class);
     job.setReducerClass(Fetcher.FetcherReducer.class);
-    job.setNumReduceTasks(1);
 
     FileOutputFormat.setOutputPath(job, segment);
     job.setOutputFormatClass(FetcherOutputFormat.class);
